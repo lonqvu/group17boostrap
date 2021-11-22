7daca6db3356343789cea2fb641a0077db1c766c
lonqvu <55783937+lonqvu@users.noreply.github.com>
2021-11-22 17:42:33 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 	$connect = mysqli_connect('mysql5037.site4now.net','a7cce8_lonqvu','Lonq02092000','db_a7cce8_lonqvu'); 
 	if($connect){
@@ -10,5 +10,3 @@
 	if($connect){
 		mysqli_query($connect,"SET NAMES 'UTF8'");
 	}
->>>>>>> 2e7845229eaec97d24bf5244cf965d29391de04a
-?>
